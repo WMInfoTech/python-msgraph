ce06b40fd94ffaaa1fb90f0e425effd6676bed38
Doug Fenstermacher <douglas.fenstermacher@gmail.com>
2020-07-01 11:41:54 -0400

    adds additional relevant keywords for packages

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
 from distutils.core import setup
 import msgraph
 
-print(vars(msgraph))
 setup(
-    name='msgraph',
+    name='python-msgraph',
     version=msgraph.__version__,
-    description='Python Wrapper to the Microsoft Graph API',
+    description='Python wrapper to the Microsoft Graph API',
     author='Doug Fenstermacher',
     author_email='dpfens@wm.edu',
-    url='https://code.wm.edu/IT/software-systems/eispippackages/msgraph',
+    url='https://github.com/WMInfoTech/python-msgraph',
     packages=['msgraph'],
-    keywords=['microsoft', 'graph', 'api', 'calendar', 'event'],
-    license='',
+    keywords=['microsoft', 'graph', 'api', 'group', 'calendar', 'event', 'site', 'list', 'listitem', 'drive', 'file'],
     install_requires=['adal>=1.2.2', 'requests>=2.12.0']
 )
